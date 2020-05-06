e0622c410df1c6cffc34130a1c2deff80d4425c4
yhmtsai <yhmtsai@gmail.com>
2020-05-06 20:45:19 +0200

    add amgx_pgm kerenl def

--- core/multigrid/amgx_pgm_kernels.hpp
+++ core/multigrid/amgx_pgm_kernels.hpp
@@ -62,12 +62,31 @@ namespace amgx_pgm {
         const matrix::Dense<_vtype> *b, matrix::Dense<_vtype> *x)
 
 
-#define GKO_DECLARE_ALL_AS_TEMPLATES                                  \
-    template <typename ValueType, typename IndexType>                 \
-    GKO_DECLARE_AMGX_PGM_RESTRICT_APPLY_KERNEL(ValueType, IndexType); \
-    template <typename ValueType, typename IndexType>                 \
-    GKO_DECLARE_AMGX_PGM_PROLONGATE_APPLY_KERNEL(ValueType, IndexType)
+#define GKO_DECLARE_AMGX_PGM_MATCH_EDGE_KERNEL(_itype)           \
+    void match_edge(std::shared_ptr<const DefaultExecutor> exec, \
+                    const Array<_itype> &strongest_neighbor,     \
+                    Array<_itype> &agg)
 
+#define GKO_DECLARE_AMGX_PGM_COUNT_UNAGG_KERNEL(_itype)           \
+    void count_unagg(std::shared_ptr<const DefaultExecutor> exec, \
+                     const Array<_itype> &agg, _itype *num_unagg)
+
+#define GKO_DECLARE_AMGX_PGM_RENUMBER_KERNEL(_itype)           \
+    void renumber(std::shared_ptr<const DefaultExecutor> exec, \
+                  Array<_itype> &agg)
+
+
+#define GKO_DECLARE_ALL_AS_TEMPLATES                                    \
+    template <typename ValueType, typename IndexType>                   \
+    GKO_DECLARE_AMGX_PGM_RESTRICT_APPLY_KERNEL(ValueType, IndexType);   \
+    template <typename ValueType, typename IndexType>                   \
+    GKO_DECLARE_AMGX_PGM_PROLONGATE_APPLY_KERNEL(ValueType, IndexType); \
+    template <typename IndexType>                                       \
+    GKO_DECLARE_AMGX_PGM_MATCH_EDGE_KERNEL(IndexType);                  \
+    template <typename IndexType>                                       \
+    GKO_DECLARE_AMGX_PGM_COUNT_UNAGG_KERNEL(IndexType);                 \
+    template <typename IndexType>                                       \
+    GKO_DECLARE_AMGX_PGM_RENUMBER_KERNEL(IndexType)
 
 }  // namespace amgx_pgm
 
--- cuda/multigrid/amgx_pgm_kernels.cu
+++ cuda/multigrid/amgx_pgm_kernels.cu
@@ -82,6 +82,29 @@ GKO_INSTANTIATE_FOR_EACH_VALUE_AND_INDEX_TYPE(
     GKO_DECLARE_AMGX_PGM_PROLONGATE_APPLY_KERNEL);
 
 
+template <typename IndexType>
+void match_edge(std::shared_ptr<const CudaExecutor> exec,
+                const Array<IndexType> &strongest_neighbor,
+                Array<IndexType> &agg) GKO_NOT_IMPLEMENTED;
+
+GKO_INSTANTIATE_FOR_EACH_INDEX_TYPE(GKO_DECLARE_AMGX_PGM_MATCH_EDGE_KERNEL);
+
+
+template <typename IndexType>
+void count_unagg(std::shared_ptr<const CudaExecutor> exec,
+                 const Array<IndexType> &agg,
+                 IndexType *num_unagg) GKO_NOT_IMPLEMENTED;
+
+GKO_INSTANTIATE_FOR_EACH_INDEX_TYPE(GKO_DECLARE_AMGX_PGM_COUNT_UNAGG_KERNEL);
+
+
+template <typename IndexType>
+void renumber(std::shared_ptr<const CudaExecutor> exec,
+              Array<IndexType> &agg) GKO_NOT_IMPLEMENTED;
+
+GKO_INSTANTIATE_FOR_EACH_INDEX_TYPE(GKO_DECLARE_AMGX_PGM_RENUMBER_KERNEL);
+
+
 }  // namespace amgx_pgm
 }  // namespace cuda
 }  // namespace kernels
